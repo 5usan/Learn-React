88e6365be0e330463bce1fb8b1eab4e3428b2bfa
5usan <susanshrestha2056@gmail.com>
2021-10-08 10:36:45 +0545

    Class Component

---
+++ Class Component/src/components/UserFinder.js
@@ -0,0 +1,74 @@
+import { Fragment, Component } from "react";
+import UsersContext from "../store/users-context";
+import classes from "./UserFinder.Module.css";
+
+import Users from "./Users";
+
+class UserFinder extends Component {
+  static contextType = UsersContext;
+
+  constructor() {
+    super();
+    this.state = {
+      filteredUsers:[],
+      searchTerm: "",
+    };
+  }
+
+  //If data are fetched from database then componentDidMount() is used. It only executes once when the app is rendered in browser.
+  //No need if in this case
+  componentDidMount() {
+    //Sent http request...
+    this.setState({filteredUsers: this.context.users})
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.searchTerm !== this.state.searchTerm)
+      this.setState({
+        filteredUsers: this.context.users.filter((user) =>
+          user.name.includes(this.state.searchTerm)
+        ),
+      });
+  }
+
+  searchChangeHandler(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <div className={classes.finder}>
+          <input type="search" onChange={this.searchChangeHandler.bind(this)} />
+        </div>
+        <Users users={this.state.filteredUsers} />
+      </Fragment>
+    );
+  }
+}
+
+// const UserFinder = () => {
+//   const [filteredUsers, setFilteredUsers] = useState(DUMMY_USERS);
+//   const [searchTerm, setSearchTerm] = useState("");
+
+//   useEffect(() => {
+//     setFilteredUsers(
+//       DUMMY_USERS.filter((user) => user.name.includes(searchTerm))
+//     );
+//   }, [searchTerm]);
+
+//   const searchChangeHandler = (event) => {
+//     setSearchTerm(event.target.value);
+//   };
+
+//   return (
+//     <Fragment>
+//       <div className={classes.finder}>
+//         <input type="search" onChange={searchChangeHandler} />
+//       </div>
+//       <Users users={filteredUsers} />
+//     </Fragment>
+//   );
+// };
+
+export default UserFinder;
