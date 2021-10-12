aafa9c0d0a68bb09d72a33a557687984b884528c
5usan <susanshrestha2056@gmail.com>
2021-10-12 09:44:30 +0545

    Basic custom hook

---
+++ Custom Hook/src/hooks/use-counter.js
@@ -0,0 +1,21 @@
+import { useState, useEffect } from "react";
+
+const useCounter = (forward = true) => {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+        if(forward){
+            setCounter((prevCtr) => prevCtr + 1);
+        }
+        else{
+            setCounter((prevCtr) => prevCtr - 1);
+        }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [forward]);
+
+  return counter;
+};
+ export default useCounter;
