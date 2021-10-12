9c608809de4e85d142532fcded649abfe44a8852
5usan <susanshrestha2056@gmail.com>
2021-10-12 23:22:26 +0545

    A simple task traker app which uses custom hook

---
+++ Realistic example of custom hook/src/App.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react';
+
+import Tasks from './components/Tasks/Tasks';
+import NewTask from './components/NewTask/NewTask';
+import useHttp from './hooks/use-http';
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+
+  //Object distructuring
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp(); //sendRequest: fetchTasks means renaming sendRequest to fetchTasks
+
+  useEffect(() => {
+    const transformTasks = (tasksObj) => {
+      const loadedTasks = [];
+
+      for (const taskKey in tasksObj) {
+        loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+      }
+
+      setTasks(loadedTasks);
+    };
+
+    fetchTasks(
+      { url: 'https://simple-task-project-default-rtdb.firebaseio.com/tasks.json' },
+      transformTasks
+    );
+  }, [fetchTasks]);
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
+  return (
+    <React.Fragment>
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={fetchTasks}
+      />
+    </React.Fragment>
+  );
+}
+
+export default App;
